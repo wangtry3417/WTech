5eb909f6efb439c0d895461f66efd1defe2730bc
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-01-06 18:46:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify,request
 
 app = Flask("WTech")
 
@@ -6,6 +6,10 @@ app = Flask("WTech")
 def index():
   return render_template("index.html")
 
+@app.route("/chat",methods=["POST"])
+def chat():
+  username = request.form.get("user")
+  return jsonify({"user":username})
 
 
 app.run(host="0.0.0.0",port=5000)
