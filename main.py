280fcab20bf0e32dd6f66def5b9f654c25b72a82
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-01-06 20:19:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,jsonify,request
+from flask import Flask,render_template,jsonify,request,abort
 
 app = Flask("WTech")
 
@@ -10,6 +10,21 @@ def index():
 def chat():
   username = request.form.get("user")
   return jsonify({"user":username})
+
+@app.route("/wcoin/login")
+def lo():
+  return render_template("wcoin.html")
+
+@app.route("/wcoin/client",methods=["POST"])
+def client():
+  user = request.form.get("user")
+  pw = request.form.get("pw")
+  if user == "wangtry" and pw == "003417":
+    return render_template("client.html",user=user)
+  elif user == "Cw1023" and pw == "1023":
+    return render_template("client.html",user=user)
+  else:
+    return abort(405)
 
 
 app.run(host="0.0.0.0",port=5000)
