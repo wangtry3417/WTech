4ae26b5440f6c5d17fc14aca6f22e4aea158f114
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-01-14 19:57:22 +0800

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -8,9 +8,8 @@ class AIModules:
     self.text = text
   def think(self):
     tokens = word_tokenize(self.text)  # 分词
-        tokens = [token.lower() for token in tokens]  # 转换为小写
-        tokens = [token for token in tokens if token.isalpha()]  # 仅保留字母字符
-        tokens = [token for token in tokens if token not in stopwords.words("english")]  # 去除停用词
-
-        response = " ".join(tokens)
-        return response
+    tokens = [token.lower() for token in tokens]  # 转换为小写
+    tokens = [token for token in tokens if token.isalpha()]  # 仅保留字母字符
+    tokens = [token for token in tokens if token not in stopwords.words("english")]  # 去除停用词
+    response = " ".join(tokens)
+    return response
