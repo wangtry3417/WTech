c27bbab6522a6c572ea265ae11b61ed044918615
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-01-14 19:44:18 +0800

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,16 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
 class AIModules:
   def __init__(self,text):
     self.text = text
   def think(self):
-    if self.text == "Hello" or self.text == "Hi" or self.text == 
+    tokens = word_tokenize(self.text)  # 分词
+        tokens = [token.lower() for token in tokens]  # 转换为小写
+        tokens = [token for token in tokens if token.isalpha()]  # 仅保留字母字符
+        tokens = [token for token in tokens if token not in stopwords.words("english")]  # 去除停用词
+
+        response = " ".join(tokens)
+        return response
