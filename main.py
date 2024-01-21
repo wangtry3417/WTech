683a7bf4f32e952d913d1b9c0edb289e1e55207b
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-01-21 19:57:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,8 @@ def client():
   pw = request.form.get("pw")
   if user == "wangtry" and pw == "003417":
     count = 200000000
-    return render_template("client.html",user=user,count=count)
+    return render_template("block.html")
+    #return render_template("client.html",user=user,count=count)
   elif user == "Cw1023" and pw == "1023":
     count = 25000000
     return render_template("client.html",user=user,count=count)
