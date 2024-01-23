0be645a6eeaf5d8c0f1bedc3d853beb59350c009
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-01-23 14:25:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def client():
     return render_template("client.html",user=user,count=count)
   elif user == "li-chen" and pw == "Abcd1234":
     count = 200.01
-    return render_template("client.html",user=user,count=count)
+    return render_template("block.html")
   else:
     return abort(405)
 
