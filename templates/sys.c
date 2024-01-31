dccc0a1261cfbfcb1ac340a03773772b6e4e0b7a
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-01-31 20:20:25 +0800

    Update sys.c

--- templates/sys.c
+++ templates/sys.c
@@ -1,1 +1,10 @@
 #include <stdio.h>
+#include "wcoin.c"
+
+int main() {
+   char network[] = Wcoin.create("Wcoin-001");
+   char coins[] = Wcoin.setCoin("wcoin");
+   int port = 90;
+   int blockID = 779;
+   Wcoin.connectSYS(0);
+}
