826a1b843a6cb4bbe6ddea43c71de4bc34c3767c
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-02-09 23:41:53 +0800

    Update trad.py

--- test/trad.py
+++ test/trad.py
@@ -1,1 +1,27 @@
 
+from wcoins.trading import Bot
+from wcoins.trade import trading,comhex,payment_method
+
+
+bot = Bot(
+    name="",
+    intents="trading"
+    ).starter()
+    
+@bot.event(event_type="create_order")
+async def create(complier : bot.Compliter):
+    price = 2300
+    coins = "Wcoins"
+    wcoin_trader = comhex(price=price,coin_name=coins,using_crypto="hash-function")
+    await complier.complex(wcoin_trader)
+    
+@bot.event(event_type="trading")
+async def trade(complier : bot.hashTrade):
+    hash = ""
+    #payment_method :
+    #["card","crypto","paypal","wpay"]
+    payment = payment_method[0]
+    wcoin_trader = hashTrade(hash).toPay(payment)
+    await complier.hashPay(wcoin_trader)
+    
+bot.run()
