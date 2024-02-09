341561b38485d4cef198ebf9438df9aa246c813c
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-02-09 23:28:34 +0800

    Update wcoin.py

--- test/wcoin.py
+++ test/wcoin.py
@@ -1,1 +1,23 @@
+from wcoins.trading import Bot
+from wcoins.trade import tradPrice
 
+
+bot = Bot(
+    name="",
+    intents="trading"
+    ).starter()
+    
+class WcoinPrice(tradPrice):
+    def __init__(self):
+        self.url = None
+        super(WcoinPrice,self).__init__()
+    def prinPrice(self):
+        self.coins = "Wcoins"
+        self.clientID = "Ws-101828"
+        self.url = f"https://wcoin.wtech.net/data/{self.coins}/{clientID}"
+        return self.url
+
+bot.run(
+   event=WcoinPrice().printPrice,
+   need_re_prin=True
+   )
