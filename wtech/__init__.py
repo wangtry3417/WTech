eb0436b6d2ad44e3cd2026eaeb16325db368dd94
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-02-15 20:32:50 +0800

    Update __init__.py

--- wtech/__init__.py
+++ wtech/__init__.py
@@ -1,4 +1,4 @@
 from .http import app
-from .auto-check import get_web_text,get_web_for_json
+from .auto_check import get_web_text,get_web_for_json
 
 __all__ = ["app","get_web_text","get_web_for_json"]
