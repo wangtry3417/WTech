8638313c00b02d165ec0f5d76fefd00ad59f032c
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-02-15 18:44:47 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,16 @@
-from setuptools import setup,find_package
-setup(name='WTech',
-  version='1.0',
-  py_modules=['WTech'],
-  author="Developer_wangtry",
-        author_email="wangtry3417@gmail.com",
-        description="Hello there,let try it out.",
-        long_description="hello there,this is make a pybot module.",
-        packages=find_packages(),
-        install_requires=["cryptography","requests"], # add any additional packages that 
-        # install_requires=[], # add any additional packages that 
-        # 需要和你的包一起安装，例如：'caer'
-        
-        keywords=['bot','WTech'],
-        classifiers= [
-            "Development Status :: 3 - Alpha",
-            "Intended Audience :: Education",
-            "Programming Language :: Python :: 2",
-            "Programming Language :: Python :: 3",
-            "Operating System :: MacOS :: MacOS X",
-            "Operating System :: Microsoft :: Windows",
-  )
+from setuptools import setup, find_packages
+
+setup(
+    name='wtech',
+    version='1.0',
+    description='That a wtech py-package.',
+    author='Wangtry',
+    author_email='wangtry3417@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'cryptography',
+        'flask',
+        'requests',
+        'random2'
+    ],
+)
