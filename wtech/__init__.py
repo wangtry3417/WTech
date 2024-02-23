4c9cba6e3389d3daec1e80738355fb6743a49dc7
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-02-23 12:20:47 +0800

    Update __init__.py

--- wtech/__init__.py
+++ wtech/__init__.py
@@ -2,4 +2,4 @@ from .http import app
 from .auto_check import get_web_text,get_web_for_json
 from .dcBot import setup_bot, command_handler
 
-__all__ = ["app","get_web_text","get_web_for_json",'setup_bot', 'command_handler']
+__all__ = ["app","get_web_text","get_web_for_json",'setup_bot', 'add_command']
