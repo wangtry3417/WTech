c19f925cfdc0c5fc1892ba3bdab616cb264f5d56
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-02-23 12:09:47 +0800

    Update bot_commands.py

--- wtech/dcBot/bot_commands.py
+++ wtech/dcBot/bot_commands.py
@@ -1,1 +1,10 @@
+from discord.ext import commands
 
+def register_command(bot, name=None):
+    def decorator(func):
+        nonlocal name
+        if name is None:
+            name = func.__name__
+        bot.add_command(commands.Command(func, name=name))
+        return func
+    return decorator
