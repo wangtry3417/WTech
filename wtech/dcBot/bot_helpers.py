de59b7907d0704f3117d7fe841d7dd238490c3fa
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-02-23 11:52:12 +0800

    Update bot_helpers.py

--- wtech/dcBot/bot_helpers.py
+++ wtech/dcBot/bot_helpers.py
@@ -1,1 +1,21 @@
+import discord
+from discord.ext import commands
 
+
+def setup_bot(prefix):
+    """
+    創建和設置一個 Discord bot。
+    
+    :param prefix: 字符串，定義命令的前綴字符。
+    :return: 返回一個設置好的 bot 實例。
+    """
+    
+    
+    bot = commands.Bot(command_prefix=prefix)
+    
+   
+    @bot.event
+    async def on_ready():
+        print(f'{bot.user.name} has connected to Discord!')
+
+    return bot
