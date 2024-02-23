2e4818e1cd0a5e2450e274c3e2bd3f5c95cd6045
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-02-23 12:28:40 +0800

    Update __init__.py

--- wtech/dcBot/__init__.py
+++ wtech/dcBot/__init__.py
@@ -1,4 +1,5 @@
 from .bot_helpers import setup_bot
 from .bot_commands import add_command
+from .bot_intents import set_all_intents
 
-__all__ = ['setup_bot', 'add_command']
+__all__ = ['setup_bot', 'add_command','set_all_intents']
