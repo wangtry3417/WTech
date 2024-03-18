e6b3911a261134101c10b42274a28eb6fd7f9020
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-03-18 14:00:37 +0800

    Update bot_embed.py

--- wtech/dcBot/bot_embed.py
+++ wtech/dcBot/bot_embed.py
@@ -1,5 +1,5 @@
 from discord import Embed
 
 def embed(*, colour=None, color=None, title=None, type='rich', url=None, description=None, timestamp=None):
-  e = Embed(colour=colour,color=color,title=title,type=type,url=url,description=description,timestamp=timeslamp)
+  e = Embed(colour=colour,color=color,title=title,type=type,url=url,description=description,timestamp=timestamp)
   return e
