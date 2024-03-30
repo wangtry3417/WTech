fcb44a81b9776e481ef7b2c21097aa6e4acc509c
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-03-30 22:29:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -204,14 +204,14 @@ def wtechMiningWcoins():
     fmat = ((adrs[1]*np_prices.mean())/np_prices.min())*2
     data = ["wtps://wcoins.wtech.net:3305/mining",fmat]
     # 将列表转换为字符串
-      list_string = str(data)
+    list_string = str(data)
 
     # 创建 Fernet 加密器
-      fernet = Fernet(key)
+    fernet = Fernet(key)
 
     # 加密字符串
-      encrypted_data =  fernet.encrypt(list_string.encode())
-      token = encrypted_data.decode()
+    encrypted_data =  fernet.encrypt(list_string.encode())
+    token = encrypted_data.decode()
     run_times = 0
     while run_times <= 10:
       run_times += 1
