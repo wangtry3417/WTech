64d0711dbd73da7e5413500d09396c9d0a69483d
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-04-02 16:32:32 +0800

    Create screenBlack.js

---
+++ static/screenBlack.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         My First User Script
+// @namespace    http://yourwebsite.com/
+// @version      0.1
+// @description  This is my first user script!
+// @include      *
+// ==/UserScript==
+(function() {
+    'use strict';
+    alert("Hello, World!");
+})();
