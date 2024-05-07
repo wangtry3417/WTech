94f65a97a25ee898dc97f1accb92a9c1b3a68784
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-05-07 18:51:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -45,6 +45,13 @@ app = Flask("WTech")
 
 CORS(app)
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
 auth = HTTPBasicAuth()
 
 users = {
