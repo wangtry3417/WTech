51440e7d03dbe21536fd00dae920ff68a60cfe84
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-05-27 11:28:51 +0800

    Update __init__.py

--- wtech/__init__.py
+++ wtech/__init__.py
@@ -1,5 +1,6 @@
 from .http import app
 from .auto_check import get_web_text,get_web_for_json
 from .dcBot import setup_bot, add_command, embed
+from .network import network
 
-__all__ = ["app","get_web_text","get_web_for_json",'setup_bot', 'add_command','embed']
+__all__ = ["app","get_web_text","get_web_for_json",'setup_bot', 'add_command','embed','network']
