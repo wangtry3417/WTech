86213b143fc2616fd7ae13e259c4203bf1991f80
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-05-31 17:07:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1063,8 +1063,8 @@ def wbank_client():
       elif pw != row[2]:
         error_message = "密碼不正確"
         break
-      elif row[0] == "wangtry":
-        error_message = "該帳戶因欠本行大量金額而被停用"
+      elif row[0] == "BenChan609":
+        error_message = "該帳戶因洗錢而被停用"
         break
       else: 
         balance = row[1]
