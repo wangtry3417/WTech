b9bcc2a64b909b629d4257d9ac2655f49856f39e
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-06-23 13:42:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ class wbankwallet(db.Model):
 admin = Admin(app, name='泓財銀行--管理介面', template_mode='bootstrap3')
 
 # 添加 SQLAlchemy 模型管理視圖
-admin.add_view(ModelView(wbankwallet, db.session, column_labels={'username':'用戶名稱','balance':'餘額(WTC)','password':'密碼'}))
+admin.add_view(ModelView(wbankwallet, db.session))
 
 @app.after_request
 def after_request(response):
