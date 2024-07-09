29f0cbd61a7611d187326a019d314fc0e327ebd9
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-07-09 11:30:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -397,6 +397,139 @@ WHERE username='{user}'""")
     conn.commit()
     emit('UpdateProfit',{'amount': profit})
 
+class wbankclients(db.Model):
+    # human readable name, not required
+    name = db.Column(db.String(40))
+
+    # human readable description, not required
+    description = db.Column(db.String(400))
+
+    # creator of the client, not required
+    user_id = db.Column(db.ForeignKey('user.id'))
+    # required if you need to support client credential
+    user = db.relationship('User')
+
+    client_id = db.Column(db.String(40), primary_key=True)
+    client_secret = db.Column(db.String(55), unique=True, index=True,
+                              nullable=False)
+
+    # public or confidential
+    is_confidential = db.Column(db.Boolean)
+
+    _redirect_uris = db.Column(db.Text)
+    _default_scopes = db.Column(db.Text)
+
+    @property
+    def client_type(self):
+        if self.is_confidential:
+            return 'verified'
+        return 'public'
+
+    @property
+    def redirect_uris(self):
+        if self._redirect_uris:
+            return self._redirect_uris.split()
+        return []
+
+    @property
+    def default_redirect_uri(self):
+        return self.redirect_uris[0]
+
+    @property
+    def default_scopes(self):
+        if self._default_scopes:
+            return self._default_scopes.split()
+        return []
+
+class wbankgrants(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(
+        db.Integer, db.ForeignKey('user.id', ondelete='CASCADE')
+    )
+    user = db.relationship('User')
+
+    client_id = db.Column(
+        db.String(40), db.ForeignKey('client.client_id'),
+        nullable=False,
+    )
+    client = db.relationship('Client')
+
+    code = db.Column(db.String(255), index=True, nullable=False)
+
+    redirect_uri = db.Column(db.String(255))
+    expires = db.Column(db.DateTime)
+
+    _scopes = db.Column(db.Text)
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return self
+
+    @property
+    def scopes(self):
+        if self._scopes:
+            return self._scopes.split()
+        return []
+
+class wbankapitoken(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    client_id = db.Column(
+        db.String(40), db.ForeignKey('client.client_id'),
+        nullable=False,
+    )
+    client = db.relationship('Client')
+
+    user_id = db.Column(
+        db.Integer, db.ForeignKey('user.id')
+    )
+    user = db.relationship('User')
+
+    # currently only bearer is supported
+    token_type = db.Column(db.String(40))
+
+    access_token = db.Column(db.String(255), unique=True)
+    refresh_token = db.Column(db.String(255), unique=True)
+    expires = db.Column(db.DateTime)
+    _scopes = db.Column(db.Text)
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return self
+
+    @property
+    def scopes(self):
+        if self._scopes:
+            return self._scopes.split()
+        return []
+
+@oauth.clientgetter
+def load_client(client_id):
+    return wbankclients.query.filter_by(client_id=client_id).first()
+
+@oauth.grantgetter
+def load_grant(client_id, code):
+    return wbankgrants.query.filter_by(client_id=client_id, code=code).first()
+
+@oauth.grantsetter
+def save_grant(client_id, code, request, *args, **kwargs):
+    # decide the expires time yourself
+    expires = datetime.datetime.utcnow() + datetime.timedelta(seconds=100)
+    grant = wbankgrants(
+        client_id=client_id,
+        code=code['code'],
+        redirect_uri=request.redirect_uri,
+        _scopes=' '.join(request.scopes),
+        user=get_current_user(),
+        expires=expires
+    )
+    db.session.add(grant)
+    db.session.commit()
+    return grant
+
+"""
 # 建立 OAuth 授權路由
 @app.route('/oauth/authorize')
 @oauth.authorize_handler
@@ -437,6 +570,7 @@ def token():
         return jsonify(oauth.create_token_response(request.headers))
     return jsonify({'error': 'Invalid credentials'}), 401
 
+"""
 
 @app.route("/")
 def index():
