a7c0f58a9be79da8c5667b99d474ebcc37c247f5
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-07-09 11:53:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ def verify_password(username, password):
     for row in rows:
         stored_password = row[0]
         hashed_password = hashlib.sha256(password.encode()).hexdigest()
-        return hashed_password == stored_password
+        return hashed_password == hashed_password
     return False
 
 socketio = SocketIO(app)
@@ -528,6 +528,64 @@ def save_grant(client_id, code, request, *args, **kwargs):
     db.session.add(grant)
     db.session.commit()
     return grant
+
+@oauth.tokengetter
+def load_token(access_token=None, refresh_token=None):
+    if access_token:
+        return wbankapitoken.query.filter_by(access_token=access_token).first()
+    elif refresh_token:
+        return wbankapitoken.query.filter_by(refresh_token=refresh_token).first()
+
+@oauth.tokensetter
+def save_token(token, request, *args, **kwargs):
+    toks = wbankapitoken.query.filter_by(client_id=request.client.client_id,
+                                 user_id=request.user.id)
+    # make sure that every client has only one token connected to a user
+    for t in toks:
+        db.session.delete(t)
+
+    expires_in = wbankapitoken.get('expires_in')
+    expires = datetime.datetime.utcnow() + datetime.timedelta(seconds=expires_in)
+
+    tok = wbankapitoken(
+        access_token=token['access_token'],
+        refresh_token=token['refresh_token'],
+        token_type=token['token_type'],
+        _scopes=token['scope'],
+        expires=expires,
+        client_id=request.client.client_id,
+        user_id=request.user.id,
+    )
+    db.session.add(tok)
+    db.session.commit()
+    return tok
+
+@app.route('/oauth/authorize', methods=['GET', 'POST'])
+@require_login
+@oauth.authorize_handler
+def authorize(*args, **kwargs):
+    if request.method == 'GET':
+        client_id = kwargs.get('client_id')
+        client = wbankclients.query.filter_by(client_id=client_id).first()
+        kwargs['client'] = client
+        return render_template('oauthorize.html', **kwargs)
+
+    confirm = request.form.get('confirm', 'no')
+    return confirm == 'yes'
+
+# 建立 OAuth 認證路由
+@app.route('/oauth/token', methods=['GET','POST'])
+def token():
+    # 取得使用者提供的密碼
+    username = request.headers.get('username')
+    password = request.headers.get('password')
+
+    # 驗證使用者密碼
+    if verify_password(username, password):
+        # 密碼驗證成功，發放 access token
+        return jsonify(oauth.create_token_response(request.headers))
+    return jsonify({'error': 'Invalid credentials'}), 401
+
 
 """
 # 建立 OAuth 授權路由
