fa89e63a0e707e3316ce1d6cd0b6ca50d8e7777d
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-07-09 11:55:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -561,7 +561,7 @@ def save_token(token, request, *args, **kwargs):
     return tok
 
 @app.route('/oauth/authorize', methods=['GET', 'POST'])
-@require_login
+@oauth.require_login
 @oauth.authorize_handler
 def authorize(*args, **kwargs):
     if request.method == 'GET':
