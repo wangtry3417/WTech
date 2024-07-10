39cab8947bfee054621c0074416e32dc7981744c
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-07-11 07:44:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -378,10 +378,10 @@ def handle_join_chat(data):
 def handle_chat_message(data):
     username = data['username']
     text = data['text']
-    target_username = data['targetUsername']
-    room = f'{username}_{target_username}'
-    chat_rooms[room].append({'username': username, 'text': text})
-    emit('chatMessage', data, room=room)
+    #target_username = data['targetUsername']
+    #room = f'{username}_{target_username}'
+    #chat_rooms[room].append({'username': username, 'text': text})
+    emit('chatMessage', data)
 
 @socketio.on('leaveChat')
 def handle_leave_chat(data):
