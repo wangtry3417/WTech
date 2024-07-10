b6a815a9c7754e12459376d47e23a6cdcdbcc496
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-07-10 21:15:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -351,7 +351,7 @@ def create_new_order(data):
   user = data["username"]
   amount = data["amount"]
   payment = data["payment"]
-  emit("placeOrder",{"username":user,"amount",amount,"payment":payment})
+  emit("placeOrder",{"username":user,"amount":amount,"payment":payment})
 
 @socketio.on('createAcc')
 def handle_create_account(data):
