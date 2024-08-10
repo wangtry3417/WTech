072f0eaaba69ed37484efcaff7e0936d1eee49c7
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-08-10 16:58:45 +0800

    Create mod.js

---
+++ test/mod.js
@@ -0,0 +1,13 @@
+const { GameManager } = require("wplay.js");
+
+const gm = new GameManager()
+               .name="VM-mod"
+               .gameName="GTL-1";
+
+gm.on("connect",(player,game)=> {
+   game.emit("welcome","歡迎加入");
+   game.ui.new(position="left-top", callback=uiBuilder);
+});
+const uiBuilder = (player,game,screen) => {
+  ui.add(inputType="text-button",fontColor=gm.color.rgb(255,255,255), fontValue="選單1",callback=null)
+};
