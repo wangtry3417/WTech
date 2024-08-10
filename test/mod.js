fdcf520388bde5d7a8adab5feee6f69309a083b0
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-08-10 17:10:03 +0800

    Update mod.js

--- test/mod.js
+++ test/mod.js
@@ -9,6 +9,9 @@ gm.on("connect",(player,game)=> {
    game.ui.new(position="left-top", callback=uiBuilder);
 });
 const uiBuilder = (player,game,screen) => {
-  ui.add(inputType="text-button",fontColor=gm.color.rgb(255,255,255), fontValue="選單1",callback=null);
+  ui.add(inputType="text-button",fontColor=gm.color.rgb(255,255,255), fontValue="生命值不變",callback=null);
   ui.add(inputType="text-button",fontColor=gm.color.rgb(255,255,255), fontValue="選單2",callback=null)
+};
+const lifeRemote = (player,game) => {
+  game.remote(player.life.setValue(100),forever=true);
 };
