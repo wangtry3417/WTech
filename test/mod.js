bf50a2b49175f928628eb933fbf77f1c2f9d05f3
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-08-10 17:27:41 +0800

    Update mod.js

--- test/mod.js
+++ test/mod.js
@@ -3,14 +3,15 @@ const { GameManager } = require("wplay.js");
 const gm = new GameManager()
                .name="VM-mod"
                .gameName="GTL-1";
+gm.setKey("F2")
 
 gm.on("connect",(player,game)=> {
    game.emit("welcome","歡迎加入");
    game.ui.new(position="left-top", callback=uiBuilder);
 });
 const uiBuilder = (player,game,screen) => {
-  ui.add(inputType="text-button",fontColor=gm.color.rgb(255,255,255), fontValue="生命值不變",callback=null);
-  ui.add(inputType="text-button",fontColor=gm.color.rgb(255,255,255), fontValue="選單2",callback=null)
+  screen.ui.add(inputType="text-button",fontColor=gm.color.rgb(255,255,255), fontValue="生命值不變",callback=lifeRemote);
+  screen.ui.add(inputType="text-button",fontColor=gm.color.rgb(255,255,255), fontValue="選單2",callback=null)
 };
 const lifeRemote = (player,game) => {
   game.remote(player.life.setValue(100),forever=true);
