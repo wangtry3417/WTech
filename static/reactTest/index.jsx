9803e04bb071114671177ea02ae3a4fdaa7b56c9
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-08-21 11:21:49 +0800

    Update index.jsx

--- static/reactTest/index.jsx
+++ static/reactTest/index.jsx
@@ -1,1 +1,38 @@
+/*
+  事前下載定React及相關庫
+  npm install react react-dom
+*/
 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const comOne = () => {
+  return (
+     <div>
+      <h1>Hello</h1>
+    </div>
+    );
+};
+
+const welcomeScreen = (props) => {
+  return (
+      <div>
+      <h1>Hello {props.name}!</h1>
+      <p>Welcome</p>
+     </div>
+    );
+};
+
+const comTwo = () => {
+  return (
+      <div>
+      <welcomeScreen name="Ben"></welcomeScreen>
+     </div>
+    );
+};
+
+//root指 在html文檔中找出div id的值
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<comOne />);
+const welcome = ReactDOM.createRoot(document.getElementById('weclomePlace'));
+welcome.render(<comTwo />);
