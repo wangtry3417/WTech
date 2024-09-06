13966f87cb3fb978b656e30ddad2898ce2e710f0
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-09-07 06:16:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -180,7 +180,7 @@ def unauthorized():
 
 conn = psycopg2.connect(database="verceldb", user="default", 
 password="Gd2MsST3QYWF", host="ep-hidden-salad-a1a7pob9-pooler.ap-southeast-1.aws.neon.tech", 
-port=5432)
+port=5432,sslmode="require")
 
 
 """
