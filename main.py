3bef6027b840f69daa7e7513b48b41f3b4764043
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-09-19 10:36:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -125,7 +125,7 @@ class wbankrecord(db.Model):
 class wbankRecordView(ModelView):
   column_list = ('username','action','time')
   can_export = True
-  page_size = 200
+  page_size = 100
 
 class wbankkyc(db.Model):
     __tablename__ = 'wbankkyc'
