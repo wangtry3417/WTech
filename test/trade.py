59a3a8629c8ad9e138ee358e22bcd0d873a4090d
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-09-25 14:26:48 +0800

    Update trade.py

--- test/trade.py
+++ test/trade.py
@@ -10,3 +10,17 @@ exchange.action(Buy(amount=10000,type="leverage"))
 
 if exchange.market.price == 200000:
   exchange.action(Sell(amount=2000,type="markets"))
+
+# 要下載泓式語法
+# pip install wlang-ext wlang
+# wlang init && wlang start-detect
+
+from wcoins.trading import Buy,Sell
+from wcoins import Exchange,data
+
+exchange = Exchange({
+  "name":"wbank",
+  "apiKey":"key"
+})
+
+exchange.action(exchange.Action -> type.buy -> Buy(amount=10000,type="leverage"))
