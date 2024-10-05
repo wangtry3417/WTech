e1f33c5ef6e3bb49692683ebc6292ccf4f11f820
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-10-05 08:04:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,6 @@ from flask_login import LoginManager, UserMixin, login_user, logout_user, login_
 from flask_oauthlib.provider import OAuth2Provider
 from wtforms.validators import DataRequired
 from wtforms import StringField
-from flask_wtf.csrf import CSRFProtect
 from flask_admin.form import BaseForm
 from flask_qrcode import QRcode
 import json,sys,threading
@@ -72,8 +71,6 @@ app.config['OAUTH_CREDENTIALS'] = {
 }
 
 QRcode(app)
-
-csrf = CSRFProtect(app)
 
 oauth = OAuth2Provider(app)
 
