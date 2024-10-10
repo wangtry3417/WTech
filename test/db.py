5ed5cd7235196846802c7520e2fbe811b5cf4a91
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-10-10 21:19:21 +0800

    Update db.py

--- test/db.py
+++ test/db.py
@@ -9,6 +9,11 @@ class User(db.Model):
     self.email = email
     self.pw = pw
 
+# Add record
+"""
+  If TryDB command:
+  Insert <table.name> -> field1,field2... -> "value1","value2"...;
+"""
 user1 = User(username="Ben",email="ben@gmail.com",pw="1234")
 user1.session.add(user1)
 user1.session.commit()
