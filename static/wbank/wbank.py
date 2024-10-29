bc6ddc85ff40cf2f0e7098580be2aa4e01b6e4fb
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-10-29 15:03:11 +0800

    Update wbank.py

--- static/wbank/wbank.py
+++ static/wbank/wbank.py
@@ -1,1 +1,20 @@
+from wbank import Controller,Intents
 
+con = Controller(apiKey="key",intents=Intents.TRADE.all())
+
+@con.on
+async def on_ready():
+  print("Is ready")
+
+@con.on(type="create-order")
+async def start_tab(bot,data):
+  payment_data = {
+    "name":"wtech",
+    "reviewer":"Ben",
+    "current":"hkd/wtc",
+    "amount":"3000"
+  }
+  pd = await data.insert(payment_data)
+  await bot.send(type="payment",data=pd)
+
+con.wait()
