0be32b6fbad973e1b8e2597664b653763eaf4559
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-11-03 13:46:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1235,7 +1235,7 @@ def wbank_read_record():
             "user": u.username,
             "action": u.action,
             "time": formatted_time, # 使用格式化後的時間
-            "times":users
+            "times":u
         }
         result.append(record)
     
