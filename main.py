dad2b94f1f348e79a4b26a45e9efbb88cf6fbc8a
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-11-03 15:02:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1227,7 +1227,8 @@ def wbank_read_record():
         return jsonify({"error": "User header is required"}), 400
 
     # 根據用戶名過濾記錄
-    users = db.session.query(wbankrecord).filter(wbankrecord.username==user).all()
+    res = db.session.execute("SELECT * FROM wbankrecord WHERE username=:username", {'username': user})
+    users = res.fetchall()  # 獲取所有結果
     result = []
 
     if not users:
