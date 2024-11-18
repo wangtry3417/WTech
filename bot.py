075b62220bd2ab46410e091ea4793ac0592989fe
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-11-18 21:00:32 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,4 +72,4 @@ async def on_ready():
 
 # 啟動 Bot
 def run_bot():
-  bot.run('MTMwODA1Mjc3MTYyNjQyMjMwNA.GjLAhS.6yYTloGVv85LDUBcPvZMWXRiWSlHnfohT8BOwo')  # 替換為您的 Discord Bot Token
+  bot.run(os.envrion.get('discordToken'))  # 替換為您的 Discord Bot Token
