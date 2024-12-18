88c1c04035d9488f53d3fcc9a4b47d8a3d8eb6d9
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2024-12-18 20:49:43 +0800

    Edit bot.py

--- bot.py
+++ bot.py
@@ -201,9 +201,10 @@ async def check_new_block():
 # 啟動 Discord Bot
 @bot.event
 async def on_ready():
+    loop = asyncio.get_event_loop()
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="WBank的運作"))
     print(f'Logged in as {bot.user}!')
-    await check_new_block()
+    loop.run_until_complete(check_new_block())
 
 # 啟動 Bot
 def run_bot():
