f3290f442573b7ab2e2d9622abd180927182dfde
wangtry3417 <110099957+wangtry3417@users.noreply.github.com>
2025-04-13 11:18:59 +0800

    Create card.js

---
+++ card.js
@@ -0,0 +1,23 @@
+const { WPay, Payment } = require("wpay.js");
+
+const wpay = new WPay()
+                .apiKey="key"
+                .scope="wbank/card-payment";
+
+wpay.on("ready", ()=> {
+  wpay.emit("paymentCreate");
+});
+wpay.on("paymentCreate", ()=> {
+  let pm = Payment(wpay, {
+      cardNumber: "xxxxx",
+      password: "xxxxx",
+      accessKey: wpay.apiKey,
+      reviewer: "benchan",
+      amount: 1000,
+      curency: "HKD"
+    });
+  wpay.redirect("wbank/cardPayment",method="PATCH",pm);
+  wpay.closeConnect();
+});
+
+wpay.startListen();
