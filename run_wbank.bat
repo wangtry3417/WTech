2d831b6c4ba7d56dac324df902d0b1b8232bb1d0
senior developer 114105 <114105@vtc.edu.hk>
2026-06-17 18:16:58 +0800

    fix: clarify portproxy mapping in batch output

--- run_wbank.bat
+++ run_wbank.bat
@@ -10,8 +10,8 @@ set HTTP_PORT=8080
 set HTTPS_PORT=8443
 
 echo [WBank] Starting server...
-echo [WBank] HTTP: 0.0.0.0:%HTTP_PORT% ^(portproxy 80^)
-echo [WBank] HTTPS: 0.0.0.0:%HTTPS_PORT% ^(portproxy 443^)
+echo [WBank] HTTP: 0.0.0.0:%HTTP_PORT% ^(portproxy 80 -> %HTTP_PORT%^)
+echo [WBank] HTTPS: 0.0.0.0:%HTTPS_PORT% ^(portproxy 443 -> %HTTPS_PORT%^)
 
 python main.py
 
